e0d283702a1c524f97a01a3bcd59ccdc2c822e51
Tyler <bergmantyler0@gmail.com>
2025-10-18 13:40:14 +1000

    starting on dataset class

---
+++ recognition/s4743458_ConvNeXt/dataset.py
@@ -0,0 +1,35 @@
+import os
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from PIL import Image
+import numpy as np
+from typing import Tuple, Optional, Callable
+
+class ADNIDataset(Dataset):
+    def __init__(self, dir, split='train'):
+        self.dir = os.path.join(dir, split)
+        self.split = split
+        self.class_to_id = {'AD': 0, 'NC': 1}
+        self.id_to_class = {0: 'AD', 1: 'NC'}
+        self.images = []
+        self._load()
+
+    def _load(self):
+        for class_name in ['AD', 'NC']:
+            class_dir = os.path.join(self.dir, class_name)
+            for img_name in os.listdir(class_dir):
+                img_path = os.path.join(class_dir, img_name)
+                label = self.class_to_id[class_name]
+                self.images.append((img_path, label))
+
+    def __len__(self) -> int:
+        return len(self.images)
+    
+    def __getitem__(self, id: int) -> Tuple[torch.Tensor, int]:
+        img_path, label = self.images[id]
+
+        image = Image.open(img_path).convert('L') #grayscale mri images
+        return image, label
+def make_dataloaders(dir, batch_size: int = 32, img_size: int = 224, num_workers: int = 4):
+    pass
