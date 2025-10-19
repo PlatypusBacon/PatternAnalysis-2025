552d40508f3efb463cbfd975895bdf9b5228a542
Tyler <bergmantyler0@gmail.com>
2025-10-19 10:22:54 +1000

    class skeleton for modules.py

--- recognition/s4743458_ConvNeXt/modules.py
+++ recognition/s4743458_ConvNeXt/modules.py
@@ -1,10 +1,28 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+if not torch.cuda.is_available():
+    print("Warning CUDA not Found. Using CPU")
+from PIL import Image
 """
 Input: []
 
 ConvNeXt notes:
 4 stages:
-
+Stage 1: resolution (64,60)
+Stage 2: resolution (32,30)
+Stage 3: resolution (16,15)
+Stage 4: resolution (8,7-8)?
 """
+
+class LayerNorm2d(nn.Module):
+    def __init__(self, norm_shape: int):
+        super().__init__()
+
+class DropPath(nn.Module):
+    def __init__(self):
+        pass
 
 class ConvNeXtBlock(nn.Module):
     """
@@ -16,6 +34,11 @@ class ConvNeXtBlock(nn.Module):
     Conv2d ->
     Layer Scale ->
     Drop Path -> o <-
-
     """
-    def __init(self, )
+    def __init__(self, in_channels):
+        pass
+
+
+class ConvNeXt(nn.Module):
+    def __init__(self):
+        pass
